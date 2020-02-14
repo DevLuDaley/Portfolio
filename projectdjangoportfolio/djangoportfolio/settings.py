a788bf7313730087674289877ac5c4fb2cf1b8ee
DevLuDaley <daleyluis@gmail.com>
2020-02-14 00:32:02 -0500

    styling and admin updates

--- projectdjangoportfolio/djangoportfolio/settings.py
+++ projectdjangoportfolio/djangoportfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangoportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'porfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'djangotango99!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR
--- projectdjangoportfolio/jobs/models.py
+++ projectdjangoportfolio/jobs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Job(models.Model):
+    # image
+    image = models.ImageField(upload_to='uploaded-images/')
+    # summary
+    intro = models.CharField(max_length=100, default='Enter Intro Here...')
+    summary = models.CharField(max_length=200)
+    category = models.CharField(
+        max_length=100, default='Enter category Here...')
+    weblink = models.CharField(max_length=200, default='Enter Url Here...')
+
+    def __str__(self):
+        return self.intro
